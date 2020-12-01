e52c492926a9acb69b3466ee73376628a2652214
Lucian Tranc <luciantranc@Lucians-MBP.phub.net.cable.rogers.com>
2020-12-01 16:57:46 -0500

    fixed overflow issue on projects page

--- src/components/ProjectsHero.js
+++ src/components/ProjectsHero.js
@@ -5,7 +5,7 @@ import './ProjectsHero.css';
 function ProjectsHero() {
   return (
     <div className='hero-container'>
-      <video src='/videos/hr-website-demo.mp4' autoPlay loop muted />
+      <video src='/videos/hr.mp4' autoPlay loop muted />
       <h1>Projects</h1>
       <p>Mobile Games, Web Apps, Algorithm Visualizations and more</p>
     </div>
