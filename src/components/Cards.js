8033c54117bbb2c25ba453cac62dd5f591a49a0d
Lucian Tranc <ltranc@uoguelph.ca>
2021-09-08 20:30:17 -0400

    added hemoworld card

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,13 +9,14 @@ function Cards() {
         <div className='cards-wrapper'>
           <ul className='cards-items'>
             <CardItem
-              src='/img/hrdemo.gif'
-              title='Hover Rocket'
-              text='Mobile game thats available on the iOS Appstore and Google Play store for free'
+              src='/img/Hemoworld.gif'
+              title='Hemoworld'
+              text='Educational mobile game for children with hemophilia. Created as a part of the DATCH project (Developmentally Appropriate Tools for Children with Hemophilia). '
               label='C#, Unity'
-              github={true}
-              githublink='https://github.com/LucianTranc/HoverRocketScripts'
-              storeLinks={true}
+              buttonname='YouTube'
+              buttonlink='https://www.youtube.com/watch?v=2w3qe4ncUpk&ab_channel=LuciTranc'
+              github={false}
+              storeLinks={false}
             />
             <CardItem
               src='/img/billiards.gif'
@@ -36,15 +37,13 @@ function Cards() {
               githublink='https://github.com/LucianTranc/SDL_Game'
             />
             <CardItem
-              src='img/svgeditor.png'
-              title='SVG Image Editor Web App'
-              text='Image editor for .SVG images files where the user can edit, scale and add or delete image elements'
-              label='C, Node.js, Javascript/HTML'
+              src='/img/hrdemo.gif'
+              title='Hover Rocket'
+              text='Mobile game thats available on the iOS Appstore and Google Play store for free'
+              label='C#, Unity'
               github={true}
-              githublink='https://github.com/LucianTranc/SVG-Image-Editor'
-              secondbutton={true}
-              buttonlink='https://svg-editor.herokuapp.com/'
-              buttonname='View&nbsp;App'
+              githublink='https://github.com/LucianTranc/HoverRocketScripts'
+              storeLinks={true}
             />
           </ul>
           <ul className='cards-items'>
@@ -60,12 +59,15 @@ function Cards() {
               buttonname='Play&nbsp;Now'
             />
             <CardItem
-              src='img/sudokusolver.gif'
-              title='Sudoku Backtracking Algorithm'
-              text='Created a visualization of a Sudoku Backtracking algorithm'
-              label='Python, Pygame'
+              src='img/svgeditor.png'
+              title='SVG Image Editor Web App'
+              text='Image editor for .SVG images files where the user can edit, scale and add or delete image elements'
+              label='C, Node.js, Javascript/HTML'
               github={true}
-              githublink='https://github.com/LucianTranc/SudokuSolver'
+              githublink='https://github.com/LucianTranc/SVG-Image-Editor'
+              secondbutton={true}
+              buttonlink='https://svg-editor.herokuapp.com/'
+              buttonname='View&nbsp;App'
             />
           </ul>
           <ul className='cards-items'>
@@ -78,14 +80,12 @@ function Cards() {
               githublink='https://github.com/LucianTranc/Personal-Website'
             />
             <CardItem
-              src='/img/Rhinopavement.png'
-              title="Rhino Pavement Inc. Website"
-              text='Website I created for a friends seal coating company'
-              label='Javascript/HTML, Bootstrap'
-              github={false}
-              secondbutton={true}
-              buttonlink='https://rhinopavement.com/'
-              buttonname='Visit&nbsp;Site'
+              src='img/sudokusolver.gif'
+              title='Sudoku Backtracking Algorithm'
+              text='Created a visualization of a Sudoku Backtracking algorithm'
+              label='Python, Pygame'
+              github={true}
+              githublink='https://github.com/LucianTranc/SudokuSolver'
             />
           </ul>
         </div>
