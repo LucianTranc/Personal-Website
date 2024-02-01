f89aa4d04c1907013811eaaf520905fb5925e994
LucianTranc <ltranc@uoguelph.ca>
2024-02-01 13:26:01 -0500

    Updated Hero to say I graduated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,7 +10,7 @@ function Hero(props) {
     <div className='hero-container' ref={props.aboutRef}>
       <p className='hero-name'>LUCIAN TRANC</p>
       <h1>Game Developer</h1>
-      <p>5th year Computer Science student at the University of Guelph with a passion for game development, graphics/shader programming and C++ development</p>
+      <p>Computer Science graduate with a passion for game development, graphics/shader programming and C++ development</p>
       <div className='hero-content-link-container'>
         <a className='hero-content-link' href='https://www.linkedin.com/in/luciantranc/' target="_blank" rel="noreferrer">LinkedIn</a>
         <p>-</p>
